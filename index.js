882fc6fd597816da1866eadbb2a3f49a62ee9ed2
easter <easterchou@gmail.com>
2023-04-27 18:51:30 +1200

    Added toggle to 'About' nav link

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
 var navLinks = document.querySelectorAll(".nav-link"),
     aboutNavLink = navLinks[0],
-    workNavLink = navLinks[1];
+    workNavLink = navLinks[1],
+    
+    aboutContent = document.getElementById("about-content"),
+    workContent = document.getElementById("work-content");
 
 aboutNavLink.addEventListener("click", function() {
-    console.log("about");
+    // Toggling the display for about content
+    aboutContent.style.display = (aboutContent.style.display === 'none') ? 'block' : 'none';
+    
+    // Once about toggling works for about, we toggle work content
+    if (aboutContent.style.display === "none") {
+        workContent.style.display = "block";
+    } else {
+        workContent.style.display = "none";
+    }
 })
 
 workNavLink.addEventListener("click", function() {
     console.log("work");
 })
 
-// when i click about,
-// i want about content to show (add open class)
-// and hide work content (remove open class)
-
-// when i click work,
-// i want work content to show (add open class)
-// and hide about content (remove open class)
+// add a lil transition
 
 
